dbcc252b3b1d592797e187152fe52b3ed7bba4cc
KuanHsun Lu <will1994@uw.edu>
2019-10-28 21:58:29 -0700

    misunderstanding of HW2 while working on hw3

--- using_function.py
+++ using_function.py
@@ -1,33 +1,20 @@
 import pandas as pd
 URL ='https://data.seattle.gov/api/views/2khk-5ukd/rows.csv?accessType=DOWNLOAD'
-df = pd.read_csv(URL)
 
-#Read dataframe columns
-df_col = sorted(df.columns)
+#read data
+def read_data(url):
+    return pd.read_csv(url)
 
-#Read dataframe columns data type
-df_col_type = list(df[df_col].dtypes)
-
-def test_create_dataframe(df):
-    df_col_test = sorted(df.columns)
-    df_col_type_test = list(df[df_col_test].dtypes)
-    
-    #count row
-    row = df.shape[0]
-    
-    #check if match the requirements
-    if (df_col_test == df_col and df_col_type_test == df_col_type and row >= 10):
-        return True
-    else:
-        return False
-
-
-#Should return True
-test_create_dataframe(df)
-
-#Test add new column into dataframe to see if match the requirements
-address = None  
-df['Address'] = address
-
-#Should return False
-test_create_dataframe(df)
+#check three requirements
+def test_create_dataframe(df, colName):
+    isValid = True #initialize return
+    if list(df.columns) != colName: #check columns
+        isValid = False
+    if len(df) < 10: #check number of rows
+        isValid = False
+    for i in df.columns: #check value data type
+        for j in range(len(df)):
+            if type(df[i][j]) != type(df[i][0]):
+                isValid = False;
+                break
+    return isValid
